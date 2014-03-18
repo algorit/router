0aca6ef1701d75ce59db85d4a1947ce1188c7fb9
Gabriel Koerich <gabrielmk@gmail.com>
2014-03-17 21:12:26 -0300

    Initial workbench commmit

---
+++ src/Algorit/Router/RouterServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Algorit\Router;
+
+use Illuminate\Support\ServiceProvider;
+
+class RouterServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('algorit/router');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
