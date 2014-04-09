ec2e8b9f0b9fc785a80c397956e6bcd9a5765016
Gabriel Koerich <gabrielmk@gmail.com>
2014-04-09 14:28:59 -0300

    Add domain router

---
+++ src/Algorit/Router/Domain.php
@@ -0,0 +1,60 @@
+<?php namespace Algorit\Router;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Domain {
+
+	/**
+	 * The request instance.
+	 *
+	 * @var \Illuminate\Http\Request
+	 */
+	protected $request;
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Algorit\Router\Domain
+	 */
+	public function __construct(Request $request)
+	{
+		$this->request = $request;
+	}
+
+	/**
+	 * Get the request.
+	 *
+	 * @return \Illuminate\Http\Request  $request
+	 */
+	public function getRequest()
+	{
+		return $this->request;
+	}
+
+	/**
+	 * Get the root path.
+	 *
+	 * @return string
+	 */
+	public function getRoot()
+	{
+		return $this->request->root();
+	}
+
+	/**
+	 * Check if current domain is the given domain.
+	 *
+	 * @return \Algorit\Router\Domain
+	 */
+	public function is($domain, Closure $callback)
+	{
+		if($domain == $this->getRoot())
+		{
+			return $callback();
+		}
+
+		return $this;
+	}
+}
