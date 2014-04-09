e3f6d060f85b16cac87ec683761c1a1424fbad55
Gabriel Koerich <gabrielmk@gmail.com>
2014-04-09 15:10:35 -0300

    Add domain test

---
+++ tests/DomainTest.php
@@ -0,0 +1,50 @@
+<?php namespace Algorit\Router\Tests;
+
+use Mockery;
+use Algorit\Router\Domain;
+
+class DomainTest extends TestCase {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function testGetRequest()
+	{
+		$request = Mockery::mock('Illuminate\Http\Request');
+
+		$domain = new Domain($request);
+
+		$this->assertInstanceOf('Illuminate\Http\Request', $domain->getRequest());
+	}
+
+	public function testGetRoot()
+	{
+		$request = Mockery::mock('Illuminate\Http\Request');
+		$request->shouldReceive('root')
+				->once()
+				->andReturn('http://example.com');
+
+		$domain = new Domain($request);
+
+		$this->assertEquals('http://example.com', $domain->getRoot());
+	}
+
+	public function testIsThatDomain()
+	{
+		$request = Mockery::mock('Illuminate\Http\Request');
+		$request->shouldReceive('root')
+				->once()
+				->andReturn('http://example.com');
+
+		$domain = new Domain($request);
+
+		$make = $domain->is('http://example.com', function()
+		{
+			return 'tested!';
+		});
+
+		$this->assertEquals($make, 'tested!');
+	}
+}
